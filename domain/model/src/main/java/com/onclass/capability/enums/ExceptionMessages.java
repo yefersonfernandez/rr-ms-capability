6af10d25bafa105b5a82e4b73bae1c73c7e4634a
Yeferson Fernandez <yeferson1500@gmail.com>
2026-01-06 16:52:44 -0500

    feat(domain): add capability listing domain model and query ports

--- domain/model/src/main/java/com/onclass/capability/enums/ExceptionMessages.java
+++ domain/model/src/main/java/com/onclass/capability/enums/ExceptionMessages.java
@@ -8,7 +8,8 @@ public enum ExceptionMessages {
     CAPABILITY_TECHNOLOGIES_REPEATED("There are repeated technologies in the list"),
     TECHNOLOGY_NOT_FOUND("Some technology IDs do not exist"),
     SAGA_COMPENSATION_ASSOCIATION_FAILURE("The capability could not be associated with the selected technologies. Please try again later."),
-    CAPABILITY_ALREADY_EXISTS("A capability with the name '%s' already exists");
+    CAPABILITY_ALREADY_EXISTS("A capability with the name '%s' already exists"),
+    WEB_CLIENT_INTERNAL_SERVER_ERROR("Internal server error in technology microservice.");
 
     private final String message;
 
--- domain/model/src/main/java/com/onclass/capability/model/capability/CapabilityWithTechnologies.java
+++ domain/model/src/main/java/com/onclass/capability/model/capability/CapabilityWithTechnologies.java
@@ -1,20 +1,23 @@
 package com.onclass.capability.model.capability;
+
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.EqualsAndHashCode;
 import java.util.List;
+import com.onclass.capability.model.technology.TechnologySummary;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
-public class Capability {
+@EqualsAndHashCode
+public class CapabilityWithTechnologies {
     private Long id;
     private String name;
     private String description;
-    private List<Long> technologyIds;
-    private Integer technologyCount;
+    private List<TechnologySummary> technologies;
 }
